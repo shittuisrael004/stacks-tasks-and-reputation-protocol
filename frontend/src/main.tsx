94acdfdfd7c3393587d8d941d583bb3accd09411
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-30 12:23:41 +0100

    mount reown globally

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { appKit } from './lib/wallet';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+appKit.mount();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
